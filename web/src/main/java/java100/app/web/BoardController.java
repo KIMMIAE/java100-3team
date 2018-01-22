a8cfe6515cf2b82eeabb1d63e3ad01a75773f15d
junghun <tkdgud9148@gmail.com>
2018-01-22 19:04:20 +0900

    .

--- web/src/main/java/java100/app/web/BoardController.java
+++ web/src/main/java/java100/app/web/BoardController.java
@@ -10,9 +10,11 @@ import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import java100.app.domain.Board;
