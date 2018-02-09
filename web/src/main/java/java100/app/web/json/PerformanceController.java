3e95a11a75010a5b79d8a72d0b602fe8bd5d8672
zerox7066 <zerox7066.gmail.com>
2018-02-09 17:33:30 +0900

    황원철 수정

--- web/src/main/java/java100/app/web/json/PerformanceController.java
+++ web/src/main/java/java100/app/web/json/PerformanceController.java
@@ -35,6 +35,7 @@ public class PerformanceController {
     
     @RequestMapping("list")
     public Object list(
+            HttpSession session,
             @RequestParam(value="pn", defaultValue="1") int pageNo,
             @RequestParam(value="ps", defaultValue="5") int pageSize,
             @RequestParam(value="words", required=false) String[] words,
@@ -63,10 +64,12 @@ public class PerformanceController {
         }
         
         HashMap<String,Object> result = new HashMap<>();
-        
+        Member member = new Member();
+        member = (Member) session.getAttribute("loginUser");
         result.put("pageNo", pageNo);
         result.put("lastPageNo", lastPageNo);
         result.put("list", performanceService.list(pageNo, pageSize, options));
+        result.put("loginUser", member);
         
         return result;
     }
