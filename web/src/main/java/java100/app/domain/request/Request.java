f157acc9da0cf91d254424e74cd6adb9c5b900b1
sorryisme <wivipp39@gmail.com>
2018-03-09 17:50:10 +0900

    커밋

--- web/src/main/java/java100/app/domain/request/Request.java
+++ web/src/main/java/java100/app/domain/request/Request.java
@@ -2,6 +2,7 @@ package java100.app.domain.request;
 
 import java.sql.Date;
 
+import java100.app.domain.member.Artist;
 import java100.app.domain.member.Member;
 
 public class Request {
@@ -16,9 +17,12 @@ public class Request {
     protected Date regDate;
     protected String mapx;
     protected String mapy;
-
+    protected String artno;
+    
+    
     protected Member writer;
-
+    protected Artist artist;
+    
     @Override
     public String toString() {
         return "Request [no=" + no + ", title=" + title + ", detailDesc=" + detailDesc + ", genre=" + genre
@@ -26,101 +30,90 @@ public class Request {
                 + ", viewCount=" + viewCount + ", regDate=" + regDate + ", mapx=" + mapx + ", mapy=" + mapy
                 + ", writer=" + writer + "]";
     }
-
+    
+    public String getArtno() {
+        return artno;
+    }
+    public void setArtno(String artno) {
+        this.artno = artno;
+    }
     public int getNo() {
         return no;
     }
-
     public void setNo(int no) {
         this.no = no;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDetailDesc() {
         return detailDesc;
     }
-
     public void setDetailDesc(String detailDesc) {
         this.detailDesc = detailDesc;
     }
-
     public String getGenre() {
         return genre;
     }
-
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
     public String getEntryDate() {
         return entryDate;
     }
-
     public void setEntryDate(String entryDate) {
         this.entryDate = entryDate;
     }
-
     public String getLocation() {
         return location;
     }
-
     public void setLocation(String location) {
         this.location = location;
     }
-
     public String getSpecialDesc() {
         return specialDesc;
     }
-
     public void setSpecialDesc(String specialDesc) {
         this.specialDesc = specialDesc;
     }
-
     public int getViewCount() {
         return viewCount;
     }
-
     public void setViewCount(int viewCount) {
         this.viewCount = viewCount;
     }
-
     public Date getRegDate() {
         return regDate;
     }
-
     public void setRegDate(Date regDate) {
         this.regDate = regDate;
     }
-
     public String getMapx() {
         return mapx;
     }
-
     public void setMapx(String mapx) {
         this.mapx = mapx;
     }
-
     public String getMapy() {
         return mapy;
     }
-
     public void setMapy(String mapy) {
         this.mapy = mapy;
     }
-
     public Member getWriter() {
         return writer;
     }
-
     public void setWriter(Member writer) {
         this.writer = writer;
+    }
+    public Artist getArtist() {
+        return artist;
+    }
+    public void setArtist(Artist artist) {
+        this.artist = artist;
     }
 
 }
